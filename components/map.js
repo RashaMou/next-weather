90c53e2bfda36b4ccc769da3d5d8054479fcd22d
Rasha Moumneh <rasha@fastmailteam.com>
2021-04-15 22:18:22 -0400

    Add mapbox style to map

--- components/map.js
+++ components/map.js
@@ -11,7 +11,7 @@ const Map = ({ lat, lng }) => {
             style={{ height: "100%", width: "100%" }}
         >
             <TileLayer
-                url="https://api.mapbox.com/styles/v1/rjmh/ckkbqic0c4fzt17ntqcy6bc43/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmptaCIsImEiOiJja2ticWgyYngwZWduMndwY2RjZmhtMjltIn0.QOeHWiCR4DqKvx-l5nzUvQ"
+                url="https://api.mapbox.com/styles/v1/rjmh/cknjodfve02vj17nxm4l7ozag/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmptaCIsImEiOiJja25qb2ZwbDAwMnExMnVwOHlrc2JnMDA4In0.5jx8qYdXib2SEZh9VXpciw"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
             />
         </MapContainer>
