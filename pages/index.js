b80808bdee739bb25599386d9924b2386779e7d2
Rasha Moumneh <rasha@fastmailteam.com>
2021-04-15 21:12:23 -0400

    change footer

--- pages/index.js
+++ pages/index.js
@@ -42,12 +42,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Powered by{" "}
-                    <img
-                        src="/vercel.svg"
-                        alt="Vercel Logo"
-                        className={styles.logo}
-                    />
+                    🚀 Rasha coded this.
                 </a>
             </footer>
         </div>
